f5d7ddb3d47e53430259b8a4015ab4cdfb207010
zaku <zaku@example.com>
2013-09-15 07:30:46 +0000

    add rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finddinner::Application.config.secret_token = '258212ca164b4def88bafb4e992eaafb65320dc3d5034c3f82f46f2a96ef7b758fec4423c1a23300a9e7a4cdd50b90a49e8dc21b53d2e282cc71b56f03078359'
